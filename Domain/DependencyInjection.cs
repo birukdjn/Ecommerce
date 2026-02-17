eeb706099a9e3284e0cc0437872552d30ed94c97
Birukdjn <birukedjn@gmail.com>
2026-02-17 12:05:25 +0300

    initial commit

---
+++ Domain/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Domain
+{
+    public static class DependencyInjection
+    {
+    
+
+    }
+}
---
+++ Persistence/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services)
+        {
+            var assembly = typeof(DependencyInjection).Assembly;
+
+            
+
+            return services;
+        }
+
+    }
+}
