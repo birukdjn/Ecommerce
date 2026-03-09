4eebf32a54ecbf7c61ec5cd2ea537b58a8844d94
Biruk Dejene <birukedjn@gmail.com>
2026-03-09 16:32:07 +0300

    feat(cart): Add IsSelected property to CartItems and remove from OrderItems

---
+++ Infrastructure/Migrations/20260309133055_AddIsSelectedToCartItems.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsSelectedToCartItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSelected",
+                table: "OrderItems");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSelected",
+                table: "CartItems",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSelected",
+                table: "CartItems");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSelected",
+                table: "OrderItems",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
